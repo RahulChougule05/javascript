0316cd2a8f733b66faad2ed87b97e680156d2651
RahulChougule05 <rahulc05.rc@gmail.com>
2023-05-30 23:09:13 +0530

    while loop
    while loop

---
+++ 08_whileLoopAssignment01.js
@@ -0,0 +1,60 @@
+
+console.log(`=====1.Print numbers from 5 to 15 by incrementing 1=====`);
+
+let i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+
+console.log(`=====2.Print numbers from 50 to 40 by decrementing 1=====`);
+
+let j=50;
+while (j>=40) {
+    console.log(j);
+    j--;
+}
+
+console.log(`=====3.Print first 15 odd numbers=====`);
+
+let k=0;
+while (k<=30) {
+    if (k%2!=0) {
+       console.log(k); 
+    }
+    k++;
+}
+
+console.log(`=====4.Print first 10 even numbers=====`);
+
+let l=0;
+while (l<20) {
+    if (l%2==0) {
+       console.log(l); 
+    }
+    l++;
+}
+
+console.log(`=====5.Print table of 5=====`);
+
+let m=5;
+while (m<=50) {
+    console.log(m);
+    m=m+5;
+}
+
+console.log(`=====6.Print table of 10=====`);
+
+let n=10;
+while (n<=100) {
+    console.log(n);
+    n=n+10;
+}
+
+console.log(`=====7.Print table of 10 in reverse order=====`);
+
+let o=100;
+while (o>=10) {
+    console.log(o);
+    o=o-10;
+}
